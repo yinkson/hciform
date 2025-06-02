bc387333c1fd6a21d0d8e7b595b122e76ae594ef
unknown <yinkson51@gmail.com>
2025-06-02 21:33:41 +0100

    new changes

---
+++ script.js
@@ -0,0 +1,18 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('XiWaokA7Ez5WcU5Wj');
+})();
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        // generate a five digit number for the contact_number variable
+        // this.contact_number.value = Math.random() * 100000 | 0;
+        // these IDs from the previous steps
+        emailjs.sendForm('contact_service', 'contact_form', this)
+            .then(function() {
+                console.log('SUCCESS!');
+            }, function(error) {
+                console.log('FAILED...', error);
+            });
+    });
+}
